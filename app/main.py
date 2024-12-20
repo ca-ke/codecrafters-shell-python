e86782f58c66395fa671b00e443210a85acbcce9
ca-ke <minhare.caique@gmail.com>
2024-12-20 12:41:39 -0300

    Adiciona saida para o REPL

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        if command == "exit 0":
+            break
+
         print(f"{command}: command not found")
 
 
