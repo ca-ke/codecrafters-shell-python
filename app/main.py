34cdc31ab46ef2b392c18ef0059a88d10c009f57
ca-ke <minhare.caique@gmail.com>
2024-12-20 14:05:01 -0300

    Adiciona comando type

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
-from ast import arg
 import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
+    known_commands = set(["echo", "type", "exit"])
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        splittedInput = input().split(" ")
-        command = splittedInput[0]
-        arguments = " ".join(splittedInput[1:])
+        splitted_input = input().split(" ")
+        command = splitted_input[0]
+        arguments = " ".join(splitted_input[1:])
 
         if command == "exit":
             break
         elif command == "echo":
             sys.stdout.write(arguments)
-            sys.stdout.write("\n")
+        elif command == "type":
+            if arguments in known_commands:
+                sys.stdout.write(f"{arguments} is a shell builtin")
+            else:
+                sys.stdout.write(f"{arguments}: not found")
         else:
-            print(f"{command}: command not found")
+            sys.stdout.write(f"{command}: command not found")
+        sys.stdout.write("\n")
 
 
 if __name__ == "__main__":
