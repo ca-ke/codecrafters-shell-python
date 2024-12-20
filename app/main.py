5ad2715e20a51681fabf2e477cb05ac7d33efbba
ca-ke <minhare.caique@gmail.com>
2024-12-20 12:35:36 -0300

    Adiciona tratamento para comando invalido

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
-    # Wait for user input
-    input()
+    sys.stdout.flush()
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
