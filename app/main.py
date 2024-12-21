905990b8a49749c800b9f2def2feefb4f5f59120
ca-ke <minhare.caique@gmail.com>
2024-12-20 22:35:39 -0300

    Adiciona type

--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
 import sys
+import os
 
 
 def main():
-    known_commands = set(["echo", "type", "exit"])
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         splitted_input = input().split(" ")
         command = splitted_input[0]
         arguments = " ".join(splitted_input[1:])
+        path_variable = os.environ["PATH"]
+        builtin_commands = {"echo", "exit", "type"}
 
         if command == "exit":
             break
         elif command == "echo":
             sys.stdout.write(arguments)
         elif command == "type":
-            if arguments in known_commands:
+            if arguments in builtin_commands:
                 sys.stdout.write(f"{arguments} is a shell builtin")
             else:
-                sys.stdout.write(f"{arguments}: not found")
+                folders = path_variable.split(":")
+                found = False
+                for folder in reversed(folders):
+                    try:
+                        dir_contents = [
+                            x for x in os.listdir(folder) if x.startswith(arguments)
+                        ]
+                        if dir_contents:
+                            sys.stdout.write(f"{arguments} is {folder}/{arguments}")
+                            found = True
+                            break
+                    except FileNotFoundError:
+                        found = False
+                if not found:
+                    sys.stdout.write(f"{arguments}: not found")
         else:
             sys.stdout.write(f"{command}: command not found")
         sys.stdout.write("\n")
