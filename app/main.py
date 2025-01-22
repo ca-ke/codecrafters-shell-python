6f147aa9aa7346308b0ef6d666c98f21d519f073
ca-ke <minhare.caique@gmail.com>
2025-01-22 12:28:31 -0300

    CLI tests (cli-test-1737559711160)

--- app/main.py
+++ app/main.py
@@ -2,10 +2,43 @@ import sys
 import os
 import glob
 import subprocess
+import readline
 
 from os.path import basename, expanduser, isfile
 from typing import Dict, Optional
 from typing import List
+
+
+class AutoCompleter:
+    def __init__(self, builtins: dict, executables: dict):
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(self.completer)
+
+        self.matches = []
+        self.commands = sorted(
+            set(list(builtins.keys()) + list(executables.keys())),
+            key=lambda x: (len(x), x),
+        )
+
+    def completer(self, text, state):
+        current_word = text.split(" ")[-1]
+
+        if state == 0:
+            if current_word:
+                self.matches = sorted(
+                    [
+                        command + " "
+                        for command in self.commands
+                        if command.startswith(current_word)
+                    ]
+                )
+            else:
+                self.matches = self.commands
+
+        try:
+            return self.matches[state]
+        except IndexError:
+            return None
 
 
 class Tokenizer:
@@ -229,6 +262,7 @@ def main():
         "type": lambda args: handle_type(args, executable_finder),
     }
 
+    AutoCompleter(builtins=builtins, executables=executable_finder.get_executables())
     while True:
         line = input("$ ").strip()
         if not line:
@@ -248,7 +282,7 @@ def main():
             directory = executables.get(command)
             if directory:
                 try:
-                    command_executor.execute(f"{directory}/{command}", arguments)
+                    command_executor.execute(command, arguments)
                 except:
                     pass
             else:
